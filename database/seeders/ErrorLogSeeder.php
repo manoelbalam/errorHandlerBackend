7fbc1a4593a56c0d7fc2791b7207ad5e35a76f6e
Manoel Isaac <manoel.balam@gmail.com>
2021-02-11 03:27:10 -0500

    fix seeder

--- database/seeders/ErrorLogSeeder.php
+++ database/seeders/ErrorLogSeeder.php
@@ -21,12 +21,26 @@ class ErrorLogSeeder extends Seeder
         for ($x = 0; $x <= 50; $x++) 
         {
             $leadId = mt_rand(100000, 999999);
-            $userId=User::all()->random(1)->first();
             $errorId=Error::all()->random(1)->first();
             $countryId=Country::all()->random(1)->first();
             DB::table('error_logs')->insert([
                 'lead_id' => $leadId,
                 'user_id' => 1,
+                'error_id' => $errorId->id,
+                'country_id' => $countryId->id,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ]);
+        }
+
+        for ($x = 0; $x <= 150; $x++) 
+        {
+            $leadId = mt_rand(100000, 999999);
+            $userId=User::all()->random(1)->first();
+            $errorId=Error::all()->random(1)->first();
+            $countryId=Country::all()->random(1)->first();
+            DB::table('error_logs')->insert([
+                'lead_id' => $leadId,
+                'user_id' => $userId->id,
                 'error_id' => $errorId->id,
                 'country_id' => $countryId->id,
                 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
